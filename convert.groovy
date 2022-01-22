fb7a458b4f4e3344f5c4f7b04bc2e784b7d6f393
Egon Willighagen <egon.willighagen@gmail.com>
2022-01-22 17:51:52 +0100

    Added first bits of node info

--- convert.groovy
+++ convert.groovy
@@ -7,6 +7,9 @@ text = new File(input).text
 println "# Reading $input ..."
 
 sbml = new XmlSlurper().parseText(text)
+  .declareNamespace(celldesigner: 'http://www.sbml.org/2001/ns/celldesigner')
+  .declareNamespace(rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#')
+  .declareNamespace(bqbiol: 'http://biomodels.net/biology-qualifiers/')
 
 println "@prefix dc:      <http://purl.org/dc/elements/1.1/> ."
 println "@prefix dcterms: <http://purl.org/dc/terms/> ."
@@ -19,6 +22,49 @@ pwURL = "https://covid19map.elixir-luxembourg.org/minerva/submap/" + sbml.model.
 println "<$pwURL>\n        rdf:type skos:Collection , wp:Pathway ;"
 println "        dc:source              \"Minerva\" ."
 println ""
+
+for (species : sbml.model.listOfSpecies.species) {
+  speciesURL = pwURL + "/species/" + species.@id;
+  specificType = ""
+  if (species.annotation.'celldesigner:extension'.'celldesigner:speciesIdentity'.'celldesigner:class') {
+    type = species.annotation.'celldesigner:extension'.'celldesigner:speciesIdentity'.'celldesigner:class'
+    if (type == "COMPLEX") specificType += ", wp:Complex"
+    if (type == "PROTEIN") specificType += ", wp:Protein"
+  }
+  println "<$speciesURL>\n        rdf:type wp:DataNode${specificType} ;"
+  ncbiDone = false
+  ensemblDone = false
+  hgncDone = false
+  mainExtDone = false
+  for (annotation : species.annotation.'rdf:RDF'.'rdf:Description'.'bqbiol:isDescribedBy') {
+    extID = annotation.'rdf:Bag'.'rdf:li'.'@rdf:resource'
+    if (("" + extID).startsWith("urn:miriam:ensembl:") && !ensemblDone) {
+      ensemblDone = true
+      ensID = ("" + extID).substring(19)
+      if (!mainExtDone) {
+        mainExtDone = true
+        println "        dc:source           \"Ensembl\" ;"
+        println "        dcterms:identifier  \"${ensID}\" ;"
+      }
+      println "        wp:bdbEnsembl       <https://identifiers.org/ensembl/${ensID}> ;"
+    } else if (("" + extID).startsWith("urn:miriam:ncbigene:") && !ncbiDone) {
+      ncbiDone = true
+      ncbiID = ("" + extID).substring(20)
+      if (!mainExtDone) {
+        mainExtDone = true
+        println "        dc:source           \"Entrez Gene\" ;"
+        println "        dcterms:identifier  \"${ncbiID}\" ;"
+      }
+      println "        wp:bdbEntrezGene    <https://identifiers.org/ncbigene/${ncbiID}> ;"
+    } else if (("" + extID).startsWith("urn:miriam:hgnc.symbol:") && !hgncDone) {
+      hgncDone = true
+      hgncID = ("" + extID).substring(23)
+      println "        rdfs:label          \"${hgncID}\" ;"
+    }
+  }
+  println "        dcterms:isPartOf    <$pwURL> ."
+  println ""
+}
 
 for (reaction : sbml.model.listOfReactions.reaction) {
   interactionURL = pwURL + "/interaction/" + reaction.@id
